b43800d4e5c48520ae4d328c57b76ef48510e84f
Orlin Genchev <ogenchev@gmail.com>
2019-12-04 16:33:44 +0200

    added additional class

---
+++ src/com/codecoda/training/Called.java
@@ -0,0 +1,7 @@
+package com.codecoda.training;
+
+public class Called {
+    public Called() {
+        System.out.println("Called constructor");
+    }
+}
--- src/com/codecoda/training/Hello.java
+++ src/com/codecoda/training/Hello.java
@@ -3,5 +3,6 @@ package com.codecoda.training;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
+        Called called = new Called();
     }
 }
