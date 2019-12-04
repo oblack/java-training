4c657f9e79cdb717e749ae5affad5a21ab8ddfba
Orlin Genchev <ogenchev@gmail.com>
2019-12-04 16:20:33 +0200

    update docker & docker compose files add basic test class

--- dockerfile
+++ dockerfile
@@ -1,5 +1,3 @@
 FROM openjdk
 EXPOSE 4567
-WORKDIR /java-training
 COPY . /java-training
-RUN mvn dependency:go-offline
---
+++ src/com/codecoda/training/Hello.java
@@ -0,0 +1,7 @@
+package com.codecoda.training;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
